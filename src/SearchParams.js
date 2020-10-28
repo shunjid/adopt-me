83d15f58009f344ebf557e9e49d2ff404165c6cc
shunjid <shunjid.rahman@gmail.com>
2020-10-29 00:39:54 +0600

    imported animal dropdown from api

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
+  const [breed, setBreed] = useState("");
+  const [breeds, setBreeds] = useState([]);
 
   return (
     <div className="search-params">
@@ -14,6 +18,40 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(e) => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map((eachAnimal) => (
+              <option key={eachAnimal} value={eachAnimal}>
+                {eachAnimal}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+            disabled={breeds.length === 0}
+          >
+            <option>All</option>
+            {breeds.map((eachBreed) => (
+              <option key={eachBreed} value={eachBreed}>
+                {eachBreed}
+              </option>
+            ))}
+          </select>
         </label>
         <button type="submit">Submit</button>
       </form>

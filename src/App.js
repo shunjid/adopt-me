1232b0be1fc7c979b8655851e639865b762cc5ea
shunjid <shunjid.rahman@gmail.com>
2021-08-28 13:24:58 +0600

    Make pure-react component setup

---
+++ src/App.js
@@ -0,0 +1,28 @@
+const Pet = ({ name, animal, breed }) => {
+    return React.createElement("div", {}, [
+        React.createElement("h2", {}, name),
+        React.createElement("p", {}, animal),
+        React.createElement("p", {}, breed),
+    ]);
+}
+
+
+const App = () => {
+    const petFeatures = [
+        { name: "Luna", animal: "Dog", breed: "Havanese" },
+        { name: "Pepper", animal: "Bird", breed: "Cockatiel" },
+        { name: "Sudo", animal: "Dog", breed: "Wheaten Terrier" },
+    ];
+
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Adopt Me!"),
+        ...petFeatures.map((petFeature) => React.createElement(Pet, petFeature)),
+    ]);
+};
+
+const render = () => {
+    const rootElement = document.getElementById("root");
+    ReactDOM.render(React.createElement(App), rootElement);
+}
+
+render();

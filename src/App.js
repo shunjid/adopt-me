f339ed289ecae2a5a9a88451ea18cff6703992d4
shunjid <shunjid.rahman@gmail.com>
2021-08-28 14:24:50 +0600

    Setup parcel and react

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const Pet = ({ name, animal, breed }) => {
   return React.createElement("div", {}, [
     React.createElement("h2", {}, name),

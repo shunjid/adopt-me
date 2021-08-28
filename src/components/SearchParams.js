5dc7d4ba2454f85867e43a6b0f6471d988fc3ade
shunjid <shunjid.rahman@gmail.com>
2021-08-28 19:39:21 +0600

    Setup hooks for select

--- src/components/SearchParams.js
+++ src/components/SearchParams.js
@@ -1,7 +1,10 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("");
 
   return (
     <div className="search-params">
@@ -14,6 +17,22 @@ const SearchParams = () => {
             value={location}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map((eachAnimal) => (
+              <option key={eachAnimal} value={eachAnimal}>
+                {eachAnimal}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>

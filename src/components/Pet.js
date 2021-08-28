fba44b71d784bf713cd4187e2c7c758f868c938b
shunjid <shunjid.rahman@gmail.com>
2021-08-28 21:35:39 +0600

    Redesign pet component

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,10 +1,20 @@
-const Pet = ({ name, animal, breed }) => {
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = "http://pets-v2.dev-apis.com/pets/none.jpg";
+
+  if (images.length) {
+    hero = images[0];
+  }
+
   return (
-    <div>
-      <h2>{name}</h2>
-      <p>{animal}</p>
-      <p>{breed}</p>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h2>{name}</h2>
+        <p>{`${animal} - ${breed} - ${location}`}</p>
+      </div>
+    </a>
   );
 };
 

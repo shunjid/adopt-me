5368ca1240d2d7c0ad28636bee081449ed5b3cda
shunjid <shunjid.rahman@gmail.com>
2021-08-29 12:19:16 +0600

    Use class component to fetch data from API

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,5 +1,44 @@
-const Details = () => {
-  return <h2>Hi lololo omg!</h2>;
-};
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
 
-export default Details;
+class Details extends Component {
+  constructor() {
+    super();
+
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    const petId = this.props.match.params.id;
+    const res = await fetch(`https://pets-v2.dev-apis.com/pets?id=${petId}`);
+    const json = await res.json();
+
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
+  }
+
+  render() {
+    const { animal, breed, city, state, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - ${breed} - ${city}, ${state}`
+          </h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Details);
